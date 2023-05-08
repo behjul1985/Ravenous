e4aef590c0a86dd61a4333f291482e73d788f395
Julio Behambari <jului.behambari@icloud.com>
2023-05-08 16:39:11 +0100

    commit changes @ 16:39

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ravenous Project - Test new merge branches
+          Ravenous Project
         </p>
         <a
           className="App-link"
